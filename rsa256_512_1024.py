769f2086d3ee0d8a27abe6b22d1cd084eed85d13
elastic <elastic7327@gmail.com>
2017-05-02 17:39:28 +0900

    first commit

---
+++ rsa256_512_1024.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+
+def main():
+    random_generator = Random.new().read
+    key = RSA.generate(1024, random_generator)
+    print(key)
+    print(key.can_encrypt())
+    print(key.has_private())
+    print('*'*50)
+
+    public_key = key.publickey()
+    enc_data = key.encrypt('Whhhattt\'s Up Danielllll?', 32)
+
+    print(enc_data)
+    print('*'*50)
+
+    print(key.decrypt(enc_data))
+
+
+
+
+
+if __name__ == "__main__":
+    main()
